68bee3a6f7eec53116250a2e8f41df7022675ad8
kalAthena <kalAthena@users.noreply.github.com>
2018-12-05 16:10:46 +0900

    1. 주석 수정
    주석 문구 일부 수정

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-/* test: abcdefghijikkk */
+/* test: code change */
 
 /* Session */
 const session = require('express-session');
